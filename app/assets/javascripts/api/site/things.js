603357bd190de91dde08e0e303ca4515fb9b7a7e
Chris LoPresto <chris@chrislopresto.com>
2015-08-19 07:57:52 -0400

    Integrate bateman-ember content from redis

--- app/assets/javascripts/api/site/things.js
+++ app/assets/javascripts/api/site/things.js
@@ -8,6 +8,10 @@ function createThing() {
       value: $('.thing-value').val()
     }
   };
+  $.ajaxPrefilter(function(options, originalOptions, xhr) {
+    var csrfToken = $("meta[name='csrf-token']").attr('content');
+    xhr.setRequestHeader('X-CSRF-Token', csrfToken);
+  });
   $.post('api/site/v0/things', requestPayload, function(response) {
     $('.thing').text(response.data.value);
   });
